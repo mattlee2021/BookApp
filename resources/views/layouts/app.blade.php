6287a461a579a6dfda249e05e2abb904e2245e0a
mattlee2021 <60365163+mattlee2021@users.noreply.github.com>
2020-10-12 02:04:54 -0400

    Initializing Blade Templates
    I created app.blade.php, which is the master template and main.blade.php, which will be the main page of the book list application.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title> App Name - @yield('title') </title>
+    </head>
+
+    <!-- Book and Author Inputs --> 
+    <header></header>  
+
+    <section></section>
+
+    <aside> </aside>
+
+    <footer></footer>
+        
+</html>
